d2ea89a36a9b8ff52b6079b99a38e6397b264992
FuRong1213-vibenshus <rf@nextkbh.dk>
2023-11-27 14:49:04 +0100

    some figures added

--- squares_step1.py
+++ squares_step1.py
@@ -2,8 +2,10 @@ import numpy as np
 import turtle
 
 fill_posibility = [0, 10, 30, 40, 101, 101]
-shapearray1 = np.arange(4).reshape(2,2)
-shapearray2 = np.arange(9).reshape(3,3)
+#fill_posibility = [0, 100, 0, 0, 0, 101]
+
+shapearray = np.arange(4).reshape(2,2)
+#shapearray2 = np.arange(9).reshape(3,3)
 
 def fill_square(x, y, width):
     t.fillcolor(np.random.randint(50, 100),
@@ -11,11 +13,11 @@ def fill_square(x, y, width):
                 np.random.randint(150, 255))
     t.goto(x,y)
     t.pendown()
-    t.begin_fill()
+    #t.begin_fill()
     for _ in range(4):
         t.forward(width)
         t.left(90)
-    t.end_fill()
+    #t.end_fill()
     t.penup()
 
 def plot_squares(width, level, x,y, N):
@@ -24,26 +26,20 @@ def plot_squares(width, level, x,y, N):
         if fill:
             fill_square(x,y,width)
         else:
-            chooseshape = np.random.randint(1,100)<50
-            if(chooseshape):
-                for (i,j), _ in np.ndenumerate(shapearray1):
-                    plot_squares(width/2, level+1, x+i*width/2,y+j*width/2, N)
-            else: 
-                for (i,j), _ in np.ndenumerate(shapearray2):
-                    plot_squares(width/3, level+1, x+i*width/3, y+j*width/3, N)
-
+            for (i,j), _ in np.ndenumerate(shapearray):
+                plot_squares(width/2, level+1, x+i*width/2,y+j*width/2, N)
+            
 t = turtle.Turtle()
 t.speed(0)
 turtle.colormode(255)
 s = turtle.Screen()
-s.setup(600,600)
-t.pencolor('white')
+s.setup(0.75,1.0)
+t.pencolor('black')
 t.penup()
 t.width(2)
 t.goto(-200, -200)
 plot_squares(500, 0, -200, -200, 5)
-
 cv = turtle.getcanvas()
-cv.postscript(file="squareoriginal.ps", colormode='color')
+cv.postscript(file="squareblackwhitesquares3.ps", colormode='color')
 turtle.done()
 
--- squares_step2.py
+++ squares_step2.py
@@ -2,8 +2,7 @@ import numpy as np
 import turtle
 
 fill_posibility = [0, 10, 30, 40, 101, 101]
-shapearray1 = np.arange(4).reshape(2,2)
-shapearray2 = np.arange(9).reshape(3,3)
+shapearray = np.arange(4).reshape(2,2)
 
 def fill_square(x, y, width):
     t.fillcolor(np.random.randint(50, 100),
@@ -24,26 +23,20 @@ def plot_squares(width, level, x,y, N):
         if fill:
             fill_square(x,y,width)
         else:
-            chooseshape = np.random.randint(1,100)<50
-            if(chooseshape):
-                for (i,j), _ in np.ndenumerate(shapearray1):
-                    plot_squares(width/2, level+1, x+i*width/2,y+j*width/2, N)
-            else: 
-                for (i,j), _ in np.ndenumerate(shapearray2):
-                    plot_squares(width/3, level+1, x+i*width/3, y+j*width/3, N)
-
+            for (i,j), _ in np.ndenumerate(shapearray):
+                plot_squares(width/2, level+1, x+i*width/2,y+j*width/2, N)
+            
 t = turtle.Turtle()
 t.speed(0)
 turtle.colormode(255)
 s = turtle.Screen()
 s.setup(600,600)
-t.pencolor('white')
+t.pencolor('black')
 t.penup()
 t.width(2)
 t.goto(-200, -200)
 plot_squares(500, 0, -200, -200, 5)
-
 cv = turtle.getcanvas()
-cv.postscript(file="squareoriginal.ps", colormode='color')
+cv.postscript(file="square2.ps", colormode='color')
 turtle.done()
 
